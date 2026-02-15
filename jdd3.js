ac09520a0d2fcf3a5e304ca33fe8c978b5d33438
orianne1251-droid <orianne1251@gmail.com>
2026-02-15 17:51:58 +0200

    Update jdd3.js

--- jdd3.js
+++ jdd3.js
@@ -132,7 +132,7 @@ function logIn(){
       const loginModalEl = document.getElementById('loginModal');
       const loginModal = bootstrap.Modal.getInstance(loginModalEl);
       if (loginModal) loginModal.hide();
-      window.location.href = '../discheck.html';
+      window.location.href = '/coffee-for-you/discheck.html';
     })
     .catch((error) => {
       console.log(error.message);
@@ -149,6 +149,7 @@ function logout(){
 function gotomake(){
   window.location.href = './make.html';
 }
+
 
 
 
